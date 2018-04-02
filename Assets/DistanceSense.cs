60ba151221e35c7e68f6e8d78812be08ea05cba8
russellxie7 <waxie@ucsd.edu>
2018-04-02 16:20:00 -0700

    added distancesense script

---
+++ Assets/DistanceSense.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceSense : MonoBehaviour {
+
+    private GameObject player;
+
+
+    // add big canvas and print size and distance.
+
+	// Use this for initialization
+	void Start () {
+        player = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
